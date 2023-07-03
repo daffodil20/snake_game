5ac06d4dd9ec0b4a8a5350aaf84f6ecd46776324
luxzhuo <luxzhuo@126.com>
2023-07-03 12:08:13 +0800

    add boundary function

--- snake.py
+++ snake.py
@@ -1,24 +1,33 @@
 import pygame
 import time
 from random import random
+DIMENSION = 400
+
+def boundary(a):
+    if a>= DIMENSION:
+        return DIMENSION-20
+    if a<0:
+        return 0
+    return a
+
 pygame.init()
-screen = pygame.display.set_mode([600,600])
+screen = pygame.display.set_mode([DIMENSION,DIMENSION])
 running = True
 snake=[]
 food_timer = time.time()
 snake_timer= time.time()
-food_x= int(600*random()/20)*20
-food_y= int(600*random()/20)*20
-head = pygame.Rect(300,300,20,20)
+food_x= int(DIMENSION*random()/20)*20
+food_y= int(DIMENSION*random()/20)*20
+head = pygame.Rect(DIMENSION/2,DIMENSION/2,20,20)
 snake.append(head)
 direction = [0]
 while running:
     screen.fill((0,0,0))
     # generate food every 6s
-    if time.time()-food_timer > 6:
-        food_x= int(600*random()/20)*20
-        food_y= int(600*random()/20)*20
-        food_timer+=6
+    if time.time()-food_timer > 12:
+        food_x= int(DIMENSION*random()/20)*20
+        food_y= int(DIMENSION*random()/20)*20
+        food_timer+=12
     food = pygame.Rect(food_x,food_y,20,20)
     pygame.draw.rect(screen,(0,255,255),food)
    
@@ -28,12 +37,16 @@ while running:
         for i in range(len(direction)):
             if direction[i] ==0:
                 snake[i][1]-=20
+                snake[i][1]=boundary(snake[i][1])
             if direction[i] ==1:
                 snake[i][0]+=20
+                snake[i][0]=boundary(snake[i][0])
             if direction[i] ==2:
                 snake[i][1]+=20
+                snake[i][1]=boundary(snake[i][1])
             if direction[i] ==3:
                 snake[i][0]-=20
+                snake[i][0]=boundary(snake[i][0])
         snake_timer+=0.5
         
     # update direction based on part ahead 
@@ -52,27 +65,32 @@ while running:
                 dir=2
                 for body in snake:
                     body[1]+=20
-                    
+                    body[1]=boundary(body[1])
+
             if event.key == pygame.K_UP:
                 dir=0
                 for body in snake:
                     body[1]-=20
+                    body[1]=boundary(body[1])
+
             if event.key == pygame.K_LEFT:
                 dir=3
                 for body in snake:
                     body[0]-=20
+                    body[0]=boundary(body[0])
+
             if event.key == pygame.K_RIGHT:
                 dir=1
                 for body in snake:
                     body[0]+=20
+                    body[0]=boundary(body[0])
+
             direction[0]=dir
     
     #eat food
     if food_x == snake[0][0] and food_y == snake[0][1]:
     
         snake.append([snake[len(snake)-1][0],snake[len(snake)-1][1]])
-        # direction[len(snake)]=direction[len(snake)-1]
-        # snake[]
         
         if direction[len(direction)-1]==0:
             snake[len(snake)-1][1]-=20
@@ -83,14 +101,16 @@ while running:
         if direction[len(direction)-1]==3:
             snake[len(snake)-1][0]-=20
         direction.append(direction[len(direction)-1])
-        food_x= int(600*random()/20)*20
-        food_y= int(600*random()/20)*20
+        food_x= int(DIMENSION*random()/20)*20
+        food_y= int(DIMENSION*random()/20)*20
 
-            # pygame.draw.rect(screen,(0,0,255),head)
 
     # draw the snake
-    for part in snake:
-        pygame.draw.rect(screen,(0,0,255),pygame.Rect(part[0],part[1],20,20))
+    for i in range(len(snake)):
+        if i==0:
+            pygame.draw.rect(screen,(255,0,0),pygame.Rect(snake[0][0],snake[0][1],20,20))
+        else:
+            pygame.draw.rect(screen,(0,0,255),pygame.Rect(snake[i][0],snake[i][1],20,20))
 
 
     # font=pygame.font.SysFont("Arial",36)
@@ -103,8 +123,8 @@ while running:
     pygame.display.update()
     # x+=20
     # y+=1
-    snake[0][0]=snake[0][0] % 600
-    snake[0][1]=snake[0][1] % 600
+    # snake[0][0]=snake[0][0] % DIMENSION
+    # snake[0][1]=snake[0][1] % DIMENSION
     # time.sleep(0.1)
     
 pygame.quit()
